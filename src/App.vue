87ecb0926e7e1e531b596d86900bb964deb7557d
Your Name <you@example.com>
2021-02-25 14:24:21 +0800

    登录页面的静态制作、input的数据校验

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,6 @@
 </template>
 
 <style lang="scss">
-#app {
-}
+//  #app {
+// }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
+const path = require('path');
 module.exports = {
   css: {
     loaderOptions: {
       sass: {
         prependData: `@import "~@/styles/main.scss";`
+      }
+    }
+  },
+  configureWebpack: (config) => {
+    config.resolve = { // 配置解析别名
+      extensions: ['.js', '.json', '.vue'],//自动添加文件名后缀
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@c': path.resolve(__dirname, './src/components'),
       }
     }
   },
